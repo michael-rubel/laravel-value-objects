a648e8988c474943da1ea47fdb08b0ff614185f9
Michael Rubél <contact@observer.name>
2022-09-19 12:56:34 +0200

    install `pest`

---
+++ tests/Pest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Test Case
+|--------------------------------------------------------------------------
+|
+| The closure you provide to your test functions is always bound to a specific PHPUnit test
+| case class. By default, that class is 'PHPUnit\Framework\TestCase'. Of course, you may
+| need to change it using the 'uses()' function to bind a different classes or traits.
+|
+*/
+
+use Illuminate\Database\Eloquent\Model;
+use MichaelRubel\ValueObjects\Tests\TestCase;
+
+uses(TestCase::class)
+    ->in('Feature', 'Unit');
+
+/*
+|--------------------------------------------------------------------------
+| Expectations
+|--------------------------------------------------------------------------
+|
+| When you're writing tests, you often need to check that values meet certain conditions. The
+| 'expect()' function gives you access to a set of 'expectations' methods that you can use
+| to assert different things. Of course, you may extend the Expectation API at any time.
+|
+*/
+
+expect()->extend('toBeOne', function () {
+    return $this->toBe(1);
+});
+
+expect()->extend('toBeModel', function () {
+    return $this->toBeInstanceOf(Model::class);
+});
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,11 +7,6 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
-
     protected function getPackageProviders($app): array
     {
         return [
