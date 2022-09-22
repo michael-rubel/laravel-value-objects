f6eca4d2cd7e8cb7381bea30dc242068d6447f28
Michael Rubél <contact@observer.name>
2022-09-22 14:32:35 +0200

    add test for command

--- tests/Unit/ValueObjectCommandTest.php
+++ tests/Unit/ValueObjectCommandTest.php
@@ -2,10 +2,25 @@
 
 namespace Olsza\ValueObjects\Tests\Feature\ValueObjects;
 
-test('can make value object using command', function () {
-    $this->artisan('make:value-object', ['name' => 'TestValueObject']);
+use Illuminate\Support\Facades\File;
 
-    $this->assertFileExists(
-        app_path('ValueObjects' . DIRECTORY_SEPARATOR . 'TestValueObject.php')
-    );
+test('can make value object using command', function () {
+    $this->artisan('make:value-object', [
+        'name' => 'TestValueObject',
+    ]);
+
+    $pathToGeneratedFile = app_path('ValueObjects' . DIRECTORY_SEPARATOR . 'TestValueObject.php');
+    $this->assertFileExists($pathToGeneratedFile);
+    $fileString = File::get($pathToGeneratedFile);
+
+    $this->assertStringContainsString('declare(strict_types=1);', $fileString);
+    $this->assertStringContainsString('use Illuminate\Support\Traits\Conditionable;', $fileString);
+    $this->assertStringContainsString('use Illuminate\Support\Traits\Macroable;', $fileString);
+    $this->assertStringContainsString('use MichaelRubel\ValueObjects\ValueObject;', $fileString);
+    $this->assertStringContainsString('@method static static make(mixed ...$values)', $fileString);
+    $this->assertStringContainsString('class TestValueObject extends ValueObject', $fileString);
+    $this->assertStringContainsString('use Macroable, Conditionable;', $fileString);
+    $this->assertStringContainsString('public function value(): string', $fileString);
+    $this->assertStringContainsString('public function toArray(): array', $fileString);
+    $this->assertStringContainsString('public function __toString(): string', $fileString);
 });
