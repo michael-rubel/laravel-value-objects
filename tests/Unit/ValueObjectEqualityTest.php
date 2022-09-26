7c0db4e3a503987bb7a775e1b2468b9bf54cb327
Michael Rubél <contact@observer.name>
2022-09-26 08:31:23 +0200

    add `notEquals` tests

--- tests/Unit/ValueObjectEqualityTest.php
+++ tests/Unit/ValueObjectEqualityTest.php
@@ -17,3 +17,17 @@ test('value objects are equal', function () {
     $this->assertFalse($vo3->equals($vo1));
     $this->assertFalse($vo3->equals($vo2));
 });
+
+test('value objects are not equal', function () {
+    $vo1 = new ClassString('Exception');
+    $vo2 = new ClassString('Exception');
+    $vo3 = new ClassString('InvalidArgumentException');
+
+    $this->assertFalse($vo1->notEquals($vo2));
+    $this->assertFalse($vo2->notEquals($vo1));
+
+    $this->assertTrue($vo1->notEquals($vo3));
+    $this->assertTrue($vo2->notEquals($vo3));
+    $this->assertTrue($vo3->notEquals($vo1));
+    $this->assertTrue($vo3->notEquals($vo2));
+});
