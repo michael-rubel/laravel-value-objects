9c2d434ac5c0824ec5d7934124676f3ba5d954b3
Michał Rubel <contact@observer.name>
2022-09-28 17:34:04 +0200

    test for decimal equality

--- tests/Unit/ValueObjectEqualityTest.php
+++ tests/Unit/ValueObjectEqualityTest.php
@@ -3,6 +3,7 @@
 namespace Olsza\ValueObjects\Tests\Feature\ValueObjects;
 
 use MichaelRubel\ValueObjects\Collection\Complex\ClassString;
+use MichaelRubel\ValueObjects\Collection\Primitive\Decimal;
 
 test('value objects are equal', function () {
     $vo1 = new ClassString('Exception');
@@ -30,4 +31,14 @@ test('value objects are not equal', function () {
     $this->assertTrue($vo2->notEquals($vo3));
     $this->assertTrue($vo3->notEquals($vo1));
     $this->assertTrue($vo3->notEquals($vo2));
+});
+
+test('decimal object equality works as expected', function () {
+    $vo1 = new Decimal('777177711191777.97999999999998', scale: 2);
+    $vo2 = new Decimal('777177711191777.97999999999998', scale: 3);
+    $vo3 = new Decimal('777177711191777.97999999999998', scale: 2);
+
+    $this->assertFalse($vo1->equals($vo2));
+    $this->assertTrue($vo1->notEquals($vo2));
+    $this->assertTrue($vo3->equals($vo1));
 });
