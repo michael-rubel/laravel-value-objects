111c1417fc59b641fad53a2c66d0b75e07b146b9
Michael Rubél <contact@observer.name>
2022-10-06 11:11:43 +0200

    add `Name` object

---
+++ src/Collection/Complex/Name.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of michael-rubel/laravel-value-objects. (https://github.com/michael-rubel/laravel-value-objects)
+ *
+ * @link https://github.com/michael-rubel/laravel-value-objects for the canonical source repository
+ * @copyright Copyright (c) 2022 Michael Rubél. (https://github.com/michael-rubel/)
+ * @license https://raw.githubusercontent.com/michael-rubel/laravel-value-objects/main/LICENSE.md MIT
+ */
+
+namespace MichaelRubel\ValueObjects\Collection\Complex;
+
+use Illuminate\Support\Stringable;
+use MichaelRubel\ValueObjects\Collection\Primitive\Text;
+use MichaelRubel\ValueObjects\ValueObject;
+
+/**
+ * "Name" object presenting a generic name.
+ *
+ * @author Michael Rubél <michael@laravel.software>
+ *
+ * @template TKey of array-key
+ * @template TValue
+ *
+ * @method static static make(string $value)
+ * @method static static from(string $value)
+ * @method static static makeOrNull(string $value)
+ *
+ * @extends ValueObject<TKey, TValue>
+ */
+class Name extends Text
+{
+    /**
+     * Create a new instance of the value object.
+     *
+     * @param  string|Stringable  $value
+     */
+    public function __construct(string|Stringable $value)
+    {
+        parent::__construct($value);
+
+        $this->sanitize();
+    }
+
+    /**
+     * Sanitize the value.
+     *
+     * @return void
+     */
+    protected function sanitize(): void
+    {
+        $this->value = str($this->value)
+            ->replaceMatches('/\p{C}+/u', '')
+            ->replace(['\r', '\n', '\t'], '')
+            ->squish()
+            ->value();
+    }
+}
