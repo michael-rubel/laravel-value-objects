8db1239af5d3fa352547c73ff84b1bd8bc56a0d7
Michael Rubél <contact@observer.name>
2022-10-31 11:34:44 +0100

    Add `Email` value object ✉

---
+++ src/Collection/Complex/Email.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * This file is part of michael-rubel/laravel-value-objects. (https://github.com/michael-rubel/laravel-value-objects)
+ *
+ * @link https://github.com/michael-rubel/laravel-value-objects for the canonical source repository
+ * @copyright Copyright (c) 2022 Michael Rubél. (https://github.com/michael-rubel/)
+ * @license https://raw.githubusercontent.com/michael-rubel/laravel-value-objects/main/LICENSE.md MIT
+ */
+
+namespace MichaelRubel\ValueObjects\Collection\Complex;
+
+use Illuminate\Support\Stringable;
+use Illuminate\Validation\Concerns\ValidatesAttributes;
+use Illuminate\Validation\ValidationException;
+use MichaelRubel\ValueObjects\Collection\Primitive\Text;
+
+/**
+ * "Email" object presenting a generic name.
+ *
+ * @author Michael Rubél <michael@laravel.software>
+ *
+ * @template TKey of array-key
+ * @template TValue
+ *
+ * @method static static make(string|Stringable $value)
+ * @method static static from(string|Stringable $value)
+ * @method static static makeOrNull(string|Stringable|null $value)
+ *
+ * @extends Text<TKey, TValue>
+ */
+class Email extends Text
+{
+    use ValidatesAttributes;
+
+    /**
+     * Create a new instance of the value object.
+     *
+     * @param  string|Stringable  $value
+     */
+    public function __construct(protected string|Stringable $value)
+    {
+        parent::__construct($this->value);
+
+        $this->validate();
+    }
+
+    /**
+     * Validate the email.
+     *
+     * @return void
+     */
+    protected function validate(): void
+    {
+        $toValidate = ['email', $this->value(), ['filter', 'dns']];
+
+        if (! $this->validateEmail(...$toValidate)) {
+            throw ValidationException::withMessages(['Your email is invalid.']);
+        }
+    }
+}
