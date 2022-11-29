ac5668de21cf4b1400473cd1f1665c28eb3d28d6
Olsza <olsza@users.noreply.github.com>
2022-11-29 14:19:28 +0100

    check precessions for float or send exception

--- src/Concerns/SanitizesNumbers.php
+++ src/Concerns/SanitizesNumbers.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace MichaelRubel\ValueObjects\Concerns;
 
+use LengthException;
+use function Psl\Str\Byte\length;
+
 trait SanitizesNumbers
 {
     /**
@@ -13,6 +16,10 @@ trait SanitizesNumbers
      */
     protected function sanitize(float|int|string|null $number): string
     {
+        if (! $this->is_good_float($number)) {
+            throw new LengthException();
+        }
+
         $number = str($number)->replace(',', '.');
 
         $dots = $number->substrCount('.');
@@ -27,5 +34,20 @@ trait SanitizesNumbers
         return $number
             ->replaceMatches('/[^0-9.]/', '')
             ->toString();
+    }
+
+    /**
+     * @param float|int|string|null $number
+     * @return bool
+     */
+    private function is_good_float(float|int|string|null $number): bool
+    {
+        if (is_float($number)) {
+            if (length((string) ($number)) >= PHP_FLOAT_DIG) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
