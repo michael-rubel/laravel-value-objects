ca663aa4318a8a9dfb61f98077d816ddccca841a
Olsza <olsza@users.noreply.github.com>
2022-12-01 13:51:12 +0100

    refactoring test 'number can change decimals'

--- tests/Unit/Primitive/NumberTest.php
+++ tests/Unit/Primitive/NumberTest.php
@@ -115,59 +115,37 @@ test('number fails when null passed', function () {
     new Number(null);
 });
 
-test('number can change decimals', function () {
-    $valueObject = new Number('111777999.97');
-    $this->assertSame('111777999.97', $valueObject->value());
-    $valueObject = new Number('111777999,97');
-    $this->assertSame('111777999.97', $valueObject->value());
-    $valueObject = new Number('111777999.99999999997', 11);
-    $this->assertSame('111777999.99999999997', $valueObject->value());
-    $valueObject = new Number('92233720368.547', 3);
-    $this->assertSame('92233720368.547', $valueObject->value());
+test('number can change decimals', function ($input, $scale, $result) {
+    $valueObject = new Number($input, $scale);
+    $this->assertSame($result, $valueObject->value());
+})->with([
+    ['111777999.97', 2, '111777999.97'],
+    ['111777999,97', 2, '111777999.97'],
+    ['111777999.99999999997', 11, '111777999.99999999997'],
+    ['92233720368.547', 3, '92233720368.547'],
 
-    $valueObject = new Number('7.1', 0);
-    $this->assertSame('7', $valueObject->value());
-    $valueObject = new Number('7.1', 1);
-    $this->assertSame('7.1', $valueObject->value());
-    $valueObject = new Number('7.11', 2);
-    $this->assertSame('7.11', $valueObject->value());
-    $valueObject = new Number('7.99', 3);
-    $this->assertSame('7.990', $valueObject->value());
-    $valueObject = new Number('70.1', 4);
-    $this->assertSame('70.1000', $valueObject->value());
-    $valueObject = new Number('71.1', 5);
-    $this->assertSame('71.10000', $valueObject->value());
-    $valueObject = new Number('17.9', 6);
-    $this->assertSame('17.900000', $valueObject->value());
-    $valueObject = new Number('11.1', 7);
-    $this->assertSame('11.1000000', $valueObject->value());
-    $valueObject = new Number('11.7', 8);
-    $this->assertSame('11.70000000', $valueObject->value());
-    $valueObject = new Number('77.77', 9);
-    $this->assertSame('77.770000000', $valueObject->value());
-    $valueObject = new Number('777.7', 10);
-    $this->assertSame('777.7000000000', $valueObject->value());
-    $valueObject = new Number('777.7', 11);
-    $this->assertSame('777.70000000000', $valueObject->value());
-    $valueObject = new Number('777.77', 12);
-    $this->assertSame('777.770000000000', $valueObject->value());
-    $valueObject = new Number('777.777', 13);
-    $this->assertSame('777.7770000000000', $valueObject->value());
-    $valueObject = new Number('7771.777', 14);
-    $this->assertSame('7771.77700000000000', $valueObject->value());
-    $valueObject = new Number('7771.7771', 15);
-    $this->assertSame('7771.777100000000000', $valueObject->value());
-    $valueObject = new Number('7771.77711', 16);
-    $this->assertSame('7771.7771100000000000', $valueObject->value());
-    $valueObject = new Number('7771.777111', 17);
-    $this->assertSame('7771.77711100000000000', $valueObject->value());
-    $valueObject = new Number('7771.7771119', 18);
-    $this->assertSame('7771.777111900000000000', $valueObject->value());
-    $valueObject = new Number('7771.77711199', 19);
-    $this->assertSame('7771.7771119900000000000', $valueObject->value());
-    $valueObject = new Number('777177711191777.99977777777777777777', 20);
-    $this->assertSame('777177711191777.99977777777777777777', $valueObject->value());
-});
+    ['7.1', 0, '7'],
+    ['7.1', 1, '7.1'],
+    ['7.11', 2, '7.11'],
+    ['7.99', 3, '7.990'],
+    ['70.1', 4, '70.1000'],
+    ['71.1', 5, '71.10000'],
+    ['17.9', 6, '17.900000'],
+    ['11.1', 7, '11.1000000'],
+    ['11.7', 8, '11.70000000'],
+    ['77.77', 9, '77.770000000'],
+    ['777.7', 10, '777.7000000000'],
+    ['777.7', 11, '777.70000000000'],
+    ['777.77', 12, '777.770000000000'],
+    ['777.777', 13, '777.7770000000000'],
+    ['7771.777', 14, '7771.77700000000000'],
+    ['7771.7771', 15, '7771.777100000000000'],
+    ['7771.77711', 16, '7771.7771100000000000'],
+    ['7771.777111', 17, '7771.77711100000000000'],
+    ['7771.7771119', 18, '7771.777111900000000000'],
+    ['7771.77711199', 19, '7771.7771119900000000000'],
+    ['777177711191777.99977777777777777777', 20, '777177711191777.99977777777777777777'],
+]);
 
 test('number can handle huge numbers', function () {
     $valueObject = new Number('111777999.97');
